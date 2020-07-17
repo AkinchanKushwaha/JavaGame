68bc10ad7cc4e30b4e0f62218d05cc8184bd2a37
AkinchanKushwaha <66404345+AkinchanKushwaha@users.noreply.github.com>
2020-07-17 13:51:59 +0530

    4th commit.

--- myFirstGame/src/com/tutorial/game/KeyInput.java
+++ myFirstGame/src/com/tutorial/game/KeyInput.java
@@ -29,18 +29,9 @@ public class KeyInput extends KeyAdapter {
 				
 			}
 			
-			if(tempObject.getId()== ID.Player2) {
-				//key events for player 2
-				
-				if(key == KeyEvent.VK_UP) tempObject.setVelY(-5);
-				if(key == KeyEvent.VK_DOWN) tempObject.setVelY(5);
-				if(key == KeyEvent.VK_RIGHT) tempObject.setVelX(5);
-				if(key == KeyEvent.VK_LEFT) tempObject.setVelX(-5);
-				
-				
-				
-			}
 		}
+		
+		if(key == KeyEvent.VK_ESCAPE)System.exit(1);
 	}
 	
 	public void keyReleased(KeyEvent e) {
@@ -59,20 +50,7 @@ public class KeyInput extends KeyAdapter {
 				if(key == KeyEvent.VK_A) tempObject.setVelX(0);
 				
 			}
-			
-			if(tempObject.getId()== ID.Player2) {
-				//key events for player 2
-				
-				if(key == KeyEvent.VK_UP) tempObject.setVelY(0);
-				if(key == KeyEvent.VK_DOWN) tempObject.setVelY(0);
-				if(key == KeyEvent.VK_RIGHT) tempObject.setVelX(0);
-				if(key == KeyEvent.VK_LEFT) tempObject.setVelX(0);
-				
-				
-				
-			}
 		}
-		
 		
 	}
 }
