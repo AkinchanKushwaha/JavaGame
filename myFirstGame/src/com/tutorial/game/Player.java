d8037329d0b62103f18614e2255482914a9d6c39
AkinchanKushwaha <66404345+AkinchanKushwaha@users.noreply.github.com>
2020-07-19 22:14:53 +0530

    6th commit.

--- myFirstGame/src/com/tutorial/game/Player.java
+++ myFirstGame/src/com/tutorial/game/Player.java
@@ -42,7 +42,7 @@ public class Player extends GameObject{
 		for(int i = 0 ; i< handler.object.size(); i++) {
 			GameObject tempObject = handler.object.get(i);
 			
-			if(tempObject.getId() == ID.BasicEnemy) { //tempObject is now basic enemy.
+			if(tempObject.getId() == ID.BasicEnemy || tempObject.getId() ==ID.FastEnemy) { //tempObject is now basic enemy.
 				if(getBounds().intersects(tempObject.getBounds())) {
 					//collision code
 					HUD.HEALTH -= 2;
